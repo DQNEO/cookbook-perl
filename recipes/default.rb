c07129d75a05d241e206360198db094b0f5e392c
DQNEO <dqneoo@gmail.com>
2014-12-06 16:52:53 +0900

    use attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe "build-essential"
 
 bash "install perl by perlbuild" do
   code <<-EOC
-   curl --silent https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - 5.20.1 /opt/perl/
+   curl --silent https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - #{node['perl']['version']} #{node['perl']['prefix']}
    EOC
   creates "#{node['perl']['prefix']}/bin/perl"
 end
