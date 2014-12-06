c08e2aac51ed494d5f1569d6c6dfda952be0528c
DQNEO <dqneoo@gmail.com>
2014-12-06 16:50:19 +0900

    install perl by perlbuild

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,8 @@
 include_recipe "build-essential"
 
-log "hello perl"
+bash "install perl by perlbuild" do
+  code <<-EOC
+   curl --silent https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - 5.20.1 /opt/perl/
+   EOC
+  creates "#{node['perl']['prefix']}/bin/perl"
+end
