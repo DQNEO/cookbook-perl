1c841b0c9ba42946896be8241f3f8951838a51b6
DQNEO <dqneoo@gmail.com>
2014-12-06 18:09:44 +0900

    fix guard condition

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,5 +4,6 @@ bash "install perl by perlbuild" do
   code <<-EOC
    curl --silent https://raw.githubusercontent.com/tokuhirom/Perl-Build/master/perl-build | perl - #{node['perl']['version']} #{node['perl']['prefix']}
    EOC
-  creates "#{node['perl']['prefix']}/bin/perl"
+
+  not_if "test -x #{node['perl']['prefix']}/bin/perl"
 end
